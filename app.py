82e318d7d26ccaf6c6f99d22d9bf21b5477cf278
vijayasm <vlg21java@gmail.com>
2026-04-21 16:37:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def home():
         .ball{
             width:60px;
             height:60px;
-            background:Green;
+            background:green;
             border-radius:50%;
             position:absolute;
             animation: bounce 4s infinite;
