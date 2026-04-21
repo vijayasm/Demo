e687d7735bcfefb4a2aa74f675f0305e6a6dce40
vijayasm <vlg21java@gmail.com>
2026-04-21 16:43:19 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
+cd /home/ec2-user/app
+sudo pkill -f "python3 app.py" || true
+sudo nohup python3 app.py > /tmp/app.log 2>&1 &
  
-cd /home/ec2-user
- 
-# stop existing app if running
-pkill -f app.py || true
- 
-# start new app in background
-nohup python3 app.py > app.log 2>&1 &
