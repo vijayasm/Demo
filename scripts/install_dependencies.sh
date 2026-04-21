11faa9bdb10b1c0388f4e05ad1255a64d2385b8d
vijayasm <vlg21java@gmail.com>
2026-04-21 15:34:21 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,13 +5,13 @@ echo "Updating system"
 sudo dnf update -y
  
 echo "Installing python"
-#sudo dnf install python3 python3-pip -y
+sudo dnf install python3 python3-pip -y
  
 echo "Installing pip properly"
-python3 -m ensurepip --upgrade || true
+#python3 -m ensurepip --upgrade || true
  
 echo "Upgrade pip"
-sudo python3 -m pip install --upgrade pip
+#sudo python3 -m pip install --upgrade pip
  
 echo "Go to app directory"
 cd /home/ec2-user/app
