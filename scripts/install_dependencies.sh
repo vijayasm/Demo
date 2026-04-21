330950d455ba9a85b5a8c82a653400f07458e8d5
vijayasm <vlg21java@gmail.com>
2026-04-21 14:48:24 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-cd /home/ec2-user/
-python3 -m pip install -r requirements.txt
+set -e
+ 
+echo "Installing python and pip"
+ 
+sudo dnf install python3 -y
+ 
+python3 -m ensurepip --upgrade || true
+ 
+python3 -m pip install --upgrade pip
+ 
+echo "Installing project dependencies"
+ 
+cd /home/ec2-user/app
+ 
+pip3 install -r requirements.txt || true
